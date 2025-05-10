f6566b68def09172ad5a3853abcb348c198e2de4
Vitalina Stepnikova <72044379+fuchsisst@users.noreply.github.com>
2025-05-10 21:52:12 +0200

    Added Lab_1 as a regular folder

---
+++ Lab_1/basic-microservice.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+from ItemClass import Item
+from functions import save_items_to_file, load_items_from_file
+
+app = Flask(__name__)
+
+@app.route("/items", methods=["GET"])
+def items_get():
+    items = load_items_from_file('items.json')
+    return jsonify([item.to_dict() for item in items]), 200
+
+
+@app.route("/items/<int:id>", methods=["GET"])
+def items_id_get(id): 
+    item = load_items_from_file('items.json', item_id=id) 
+    if item:
+        return jsonify(item.to_dict()), 200 
+    else:
+        return jsonify({"error": "Item not found"}), 404  
+
+@app.route("/items", methods=["POST"])
+def create_item():
+    data = request.get_json()
+    items = load_items_from_file('items.json')
+    new_id = max(item.id for item in items) + 1 if items else 1
+    new_item = Item(new_id, data['name'])
+    items.append(new_item)
+    save_items_to_file(items, 'items.json')
+    return jsonify(new_item.to_dict()), 201
+
+@app.route("/items/<int:id>", methods=["PUT"])
+def update_item(id):
+    items = load_items_from_file('items.json')
+    item_to_update = next((item for item in items if item.id == id), None)
+
+    if not item_to_update:
+        return jsonify({"error": "Item not found"}), 404
+
+    data = request.get_json()
+    item_to_update.name = data['name']
+    save_items_to_file(items, 'items.json')
+    return jsonify(item_to_update.to_dict()), 200
+
+@app.route("/items/<int:id>", methods=["DELETE"])
+def delete_item(id):
+    items = load_items_from_file('items.json')
+    item_to_delete = next((item for item in items if item.id == id), None)
+
+    if not item_to_delete:
+        return jsonify({"error": "Item not found"}), 404
+
+    items.remove(item_to_delete)
+    save_items_to_file(items, 'items.json')
+    return jsonify({"message": "Item deleted successfully"}), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+
+
---
+++ Lab_1/functions.py
@@ -0,0 +1,20 @@
+import json
+from ItemClass import Item
+
+
+def save_items_to_file(items, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump([item.to_dict() for item in items], f, indent=4)
+
+def load_items_from_file(filename, item_id=None):
+    with open(filename, 'r', encoding='utf-8') as f:
+        items_data = json.load(f)
+        
+        if item_id is not None:
+            for item in items_data:
+                if item['id'] == item_id:
+                    return Item(item['id'], item['name'])
+            return None  
+        
+        return [Item(item['id'], item['name']) for item in items_data]
+
